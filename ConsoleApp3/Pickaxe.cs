edb601867131b3444d09d0ae9c363d5fe9c8ad88
BigKumpel <100943901+BigKumpel@users.noreply.github.com>
2022-06-09 21:19:06 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp3/Pickaxe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LolClasses
+{
+    internal class Pickaxe : Item
+    {
+        public Pickaxe()
+        {
+            name = "Pickaxe";
+            price = 875;
+            AddStat(Stat.AD, 25);
+        }
+    }
+}
